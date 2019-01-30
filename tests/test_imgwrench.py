2f0ef4268de42c35a9a57824ff249dc3be96cfad
luphord <luphord@protonmail.com>
2019-01-30 07:01:00 +0100

    test pipeline functionality

--- tests/test_imgwrench.py
+++ tests/test_imgwrench.py
@@ -9,6 +9,8 @@ from click.testing import CliRunner
 
 from imgwrench import cli_imgwrench
 
+from .images import pixel1x1
+
 
 class TestImgwrenchMainCli(unittest.TestCase):
     '''Tests for `imgwrench` main cli.'''
@@ -37,3 +39,36 @@ class TestImgwrenchMainCli(unittest.TestCase):
         help_result = self.runner.invoke(cli_imgwrench, ['--help'])
         for subcommand in cli_imgwrench.commands:
             self.assertIn(subcommand, help_result.output)
+
+    def test_pipeline(self):
+        '''Test pipeline functionality'''
+        self.assertTrue(cli_imgwrench.chain)
+        pipelinetest = 'pipelinetest'
+
+        n_invocations = [0]
+        n_image_invocations = [0]
+
+        @cli_imgwrench.command(pipelinetest)
+        def _pipelintest():
+            n_invocations[0] += 1
+
+            def _process(images):
+                for orgfname, image in images:
+                    n_image_invocations[0] += 1
+                    yield orgfname, image
+
+            return _process
+
+        with self.runner.isolated_filesystem():
+            with open('pixel1x1.jpg', 'wb') as f:
+                f.write(pixel1x1)
+            with open('images.txt', 'w') as f:
+                for i in range(10):
+                    f.write('pixel1x1.jpg\n')
+            result = self.runner.invoke(cli_imgwrench,
+                                        ['-i', 'images.txt', pipelinetest,
+                                         pipelinetest, pipelinetest])
+            self.assertEqual(0, result.exit_code)
+            self.assertEqual(3, n_invocations[0], 'expecting 3 processors')
+            self.assertEqual(30, n_image_invocations[0], 'expecting 30 calls')
+        del cli_imgwrench.commands[pipelinetest]
