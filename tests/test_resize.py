40a0646a7ef77fac1fb572acf3ae2025ed6927b0
luphord <luphord@protonmail.com>
2019-01-30 17:08:48 +0100

    test for image resize

---
+++ tests/test_resize.py
@@ -0,0 +1,20 @@
+'''Test `resize` command.'''
+
+import unittest
+
+from imgwrench.resize import resize
+
+from .images import pixel1x1_img
+
+
+class TestResize(unittest.TestCase):
+    '''Test for `resize` command.'''
+
+    def test_resze_size(self):
+        '''Test output sizes of resize.'''
+        pixel100 = resize(pixel1x1_img, 100)
+        self.assertEqual(100, pixel100.size[0])
+        self.assertEqual(100, pixel100.size[1])
+        pixel67 = resize(pixel100, 67)
+        self.assertEqual(67, pixel67.size[0])
+        self.assertEqual(67, pixel67.size[1])
