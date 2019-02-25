be8c7ca5010fcedd5fb4d485bc288a185b837de7
luphord <luphord@protonmail.com>
2019-02-25 17:01:38 +0100

    fix typo

--- tests/test_resize.py
+++ tests/test_resize.py
@@ -10,7 +10,7 @@ from .images import pixel1x1_img
 class TestResize(unittest.TestCase):
     '''Test for `resize` command.'''
 
-    def test_resze_size(self):
+    def test_resize_size(self):
         '''Test output sizes of resize.'''
         pixel100 = resize(pixel1x1_img, 100)
         self.assertEqual(100, pixel100.size[0])
