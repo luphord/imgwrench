258e0b7c0c5f422b13804f925aa203094dacc1d7
luphord <luphord@protonmail.com>
2019-03-13 17:06:58 +0100

    extract util method for integration test

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -4,13 +4,10 @@
 '''Tests for `stack` subcommand.'''
 
 
-import os
 import unittest
 from click.testing import CliRunner
 
-from imgwrench import cli_imgwrench
-
-from .images import pixel1x1
+from .utils import execute_and_test_output_images
 
 
 class TestStack(unittest.TestCase):
@@ -22,19 +19,5 @@ class TestStack(unittest.TestCase):
 
     def test_stacked_output(self):
         '''Test output of stack command'''
-        prefix = 'stacked_'
-        n_output = 3
-        with self.runner.isolated_filesystem():
-            with open('pixel1x1.jpg', 'wb') as f:
-                f.write(pixel1x1)
-            with open('images.txt', 'w') as f:
-                for i in range(n_output * 2):
-                    f.write('pixel1x1.jpg\n')
-            result = self.runner.invoke(cli_imgwrench,
-                                        ['-p', prefix,
-                                         '-d', 4,
-                                         '-i', 'images.txt', 'stack'])
-            self.assertEqual(0, result.exit_code)
-            for i in range(n_output):
-                fname = '{}{:04d}.jpg'.format(prefix, i)
-                self.assertTrue(os.path.exists(fname), fname + ' missing')
+        execute_and_test_output_images(self, self.runner, 6, 3,
+                                       '_stacked', ['stack'])
---
+++ tests/utils.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+'''Test utils.'''
+
+import os
+
+from imgwrench import cli_imgwrench
+
+from .images import pixel1x1
+
+
+def execute_and_test_output_images(test, cli_runner, n_input, n_output,
+                                   prefix, args):
+    '''Execute subcommand and test output images'''
+    with cli_runner.isolated_filesystem():
+        with open('pixel1x1.jpg', 'wb') as f:
+            f.write(pixel1x1)
+        with open('images.txt', 'w') as f:
+            for i in range(n_input):
+                f.write('pixel1x1.jpg\n')
+        result = cli_runner.invoke(cli_imgwrench,
+                                   ['-p', prefix,
+                                    '-d', 4,
+                                    '-i', 'images.txt'] + args)
+        test.assertEqual(0, result.exit_code)
+        for i in range(n_output):
+            fname = '{}{:04d}.jpg'.format(prefix, i)
+            exists = os.path.exists(fname)
+            test.assertTrue(exists, fname + ' missing')
