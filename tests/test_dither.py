f7b509a4005fda33c8f3e44f8124c453722da0f2
luphord <luphord@protonmail.com>
2019-07-09 17:14:27 +0200

    test for dithering

---
+++ tests/test_dither.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''Tests for `dither` subcommand.'''
+
+
+import unittest
+
+from click.testing import CliRunner
+
+from .utils import execute_and_test_output_images
+
+
+class TestDither(unittest.TestCase):
+    '''Tests for `dither` subcommand.'''
+
+    def test_dither_output(self):
+        '''Test output of dither command.'''
+        execute_and_test_output_images(self, CliRunner(), 3, 3,
+                                       'dither_', ['dither'])
