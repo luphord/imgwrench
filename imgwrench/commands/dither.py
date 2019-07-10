360dd28d76f1ba5fb386f93a2ac19f1a63e004fe
luphord <luphord@protonmail.com>
2019-07-10 06:41:07 +0200

    expose brightness factor for dithering

--- imgwrench/commands/dither.py
+++ imgwrench/commands/dither.py
@@ -4,19 +4,23 @@ import click
 from PIL import ImageEnhance
 
 
-def dither(image):
+def dither(image, brightness_factor):
     '''Apply black-white dithering to images.'''
-    image = ImageEnhance.Brightness(image).enhance(1.5)
+    image = ImageEnhance.Brightness(image).enhance(brightness_factor)
     return image.convert('1')
 
 
 @click.command(name='dither')
-def cli_dither():
+@click.option('-b', '--brightness-factor', type=click.FLOAT, default=1.5,
+              show_default=True,
+              help='adjust brightness before dithering ' +
+                   '(1.0 is neutral, larger is brighter, smaller is darker)')
+def cli_dither(brightness_factor):
     '''Apply black-white dithering to images.'''
     click.echo('Initializing dither with parameters {}'.format(locals()))
 
     def _dither(images):
         for info, image in images:
-            yield info, dither(image)
+            yield info, dither(image, brightness_factor)
 
     return _dither
--- tests/test_dither.py
+++ tests/test_dither.py
@@ -17,4 +17,4 @@ class TestDither(unittest.TestCase):
     def test_dither_output(self):
         '''Test output of dither command.'''
         execute_and_test_output_images(self, CliRunner(), 3, 3,
-                                       'dither_', ['dither'])
+                                       'dither_', ['dither', '-b', '1.6'])
