361a22ee370a731b3389e4463fc73029100f633a
luphord <luphord@protonmail.com>
2020-02-17 17:03:15 +0100

    regression test for colorfix images

--- tests/test_colorfix.py
+++ tests/test_colorfix.py
@@ -5,12 +5,15 @@
 
 
 import unittest
+from io import BytesIO
+from base64 import encodebytes
 
 from click.testing import CliRunner
-from imgwrench.commands.colorfix import quantiles
+from imgwrench.commands.colorfix import quantiles, colorfix
 
 from .utils import execute_and_test_output_images
-from .images import colorcast_img
+from .images import colorcast_img, colorcast_fixed_001, colorcast_fixed_002, \
+    colorcast_fixed_003, colorcast_fixed_005
 
 
 # targets for quantile regression test: (level, target)
@@ -20,6 +23,25 @@ QUANTILES_TARGETS = [
     (0.03, ((171, 247), (60, 213), (37, 200))),
     (0.05, ((174, 244), (69, 208), (44, 195)))
 ]
+
+
+# targets for colorfix algorithm regression test: (level, target)
+IMAGES_TARGETS = [
+    (0.01, colorcast_fixed_001),
+    (0.02, colorcast_fixed_002),
+    (0.03, colorcast_fixed_003),
+    (0.05, colorcast_fixed_005)
+]
+
+
+def _tobytes(img):
+    b = BytesIO()
+    img.save(b, format='JPEG')
+    return b.getvalue()
+
+
+def _b64encode(img):
+    return encodebytes(_tobytes(img)).decode('ascii')
 
 
 class TestBlackwhite(unittest.TestCase):
@@ -33,4 +55,11 @@ class TestBlackwhite(unittest.TestCase):
     def test_quantiles(self):
         '''Regression test for quantiles.'''
         for level, target in QUANTILES_TARGETS:
-            self.assertEqual(target, quantiles(colorcast_img, level))
+            self.assertEqual(target, quantiles(colorcast_img, level),
+                             'level {} fail'.format(level))
+
+    def test_colorfix_regression(self):
+        '''Regression test for colorfix algorithm.'''
+        for level, target in IMAGES_TARGETS:
+            cf = colorfix(colorcast_img.copy(), level)
+            self.assertEqual(target, _tobytes(cf), 'level {} fail'.format(level))
