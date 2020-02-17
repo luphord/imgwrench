dc9704f402725eed6ca7136bae89752a3376c5a6
luphord <luphord@protonmail.com>
2020-02-17 06:49:56 +0100

    regression test for colorfix quantiles

--- tests/test_colorfix.py
+++ tests/test_colorfix.py
@@ -7,8 +7,19 @@
 import unittest
 
 from click.testing import CliRunner
+from imgwrench.commands.colorfix import quantiles
 
 from .utils import execute_and_test_output_images
+from .images import colorcast_img
+
+
+# targets for quantile regression test: (level, target)
+QUANTILES_TARGETS = [
+    (0.01, ((167, 252), (53, 218), (32, 207))),
+    (0.02, ((169, 249), (56, 215), (35, 202))),
+    (0.03, ((171, 247), (60, 213), (37, 200))),
+    (0.05, ((174, 244), (69, 208), (44, 195)))
+]
 
 
 class TestBlackwhite(unittest.TestCase):
@@ -18,3 +29,8 @@ class TestBlackwhite(unittest.TestCase):
         '''Test output of colorfix command.'''
         execute_and_test_output_images(self, CliRunner(), 3, 3,
                                        'colorfix_', ['colorfix'])
+
+    def test_quantiles(self):
+        '''Regression test for quantiles.'''
+        for level, target in QUANTILES_TARGETS:
+            self.assertEqual(target, quantiles(colorcast_img, level))
