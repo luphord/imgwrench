96b5dc9c595477db20fc61c1196dd27b03f5fb56
luphord <luphord@protonmail.com>
2020-02-19 07:05:29 +0100

    colorfix algorithm using numpy

--- imgwrench/commands/colorfix.py
+++ imgwrench/commands/colorfix.py
@@ -2,6 +2,16 @@
 range.'''
 
 import click
+from PIL import Image
+import numpy as np
+
+
+nmax = np.maximum
+nmin = np.minimum
+
+
+def aint(arr):
+    return arr.astype(int)
 
 
 def _quantiles_iter(img, level):
@@ -38,16 +48,12 @@ def quantiles(img, level=0.01):
 def colorfix(img, level=0.01):
     '''Fix colors by stretching channels histograms to full range.'''
     qr, qg, qb = quantiles(img, level)
-    pixels = img.load()
-    # for every pixel
-    for i in range(img.size[0]):
-        for j in range(img.size[1]):
-            r, g, b = pixels[i, j]
-            r_new = max(min(int((r - qr[0]) / (qr[1] - qr[0]) * 256), 255), 0)
-            g_new = max(min(int((g - qg[0]) / (qg[1] - qg[0]) * 256), 255), 0)
-            b_new = max(min(int((b - qb[0]) / (qb[1] - qb[0]) * 256), 255), 0)
-            pixels[i, j] = (r_new, g_new, b_new)
-    return img
+    arr = np.array(img).astype(int)  # arr.shape = (height, width, channel)
+    r, g, b = arr[:, :, 0], arr[:, :, 1], arr[:, :, 2]
+    r[:, :] = nmax(nmin(aint((r - qr[0]) / (qr[1] - qr[0]) * 256), 255), 0)
+    g[:, :] = nmax(nmin(aint((g - qg[0]) / (qg[1] - qg[0]) * 256), 255), 0)
+    b[:, :] = nmax(nmin(aint((b - qb[0]) / (qb[1] - qb[0]) * 256), 255), 0)
+    return Image.fromarray(arr.astype('uint8'))
 
 
 @click.command(name='colorfix')
