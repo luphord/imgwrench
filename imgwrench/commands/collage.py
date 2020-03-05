dbacaac5517b8d164c74d7e2926a99b79e80156b
luphord <luphord@protonmail.com>
2020-03-05 21:14:12 +0100

    layout tree structure for calculation positions

--- imgwrench/commands/collage.py
+++ imgwrench/commands/collage.py
@@ -1,13 +1,60 @@
 '''Create a collage from multiple images.'''
 
 import click
+from PIL import Image
 
 from ..param import COLOR
 
 
+class Branch:
+    def __init__(self, content):
+        self.content = list(content)
+
+    @property
+    def normalized_content(self):
+        total = sum(w for w, _ in self.content)
+        return [(w / total, node) for w, node in self.content]
+
+
+class Row(Branch):
+    def positions(self, x, y, width, height):
+        offset = 0.0
+        for w, node in self.normalized_content:
+            total_width = w * width
+            yield from node.positions(x + offset, y, total_width, height)
+            offset += total_width
+
+
+class Column(Branch):
+    def positions(self, x, y, width, height):
+        offset = 0.0
+        for w, node in self.normalized_content:
+            total_height = w * height
+            yield from node.positions(x, y + offset, width, total_height)
+            offset += total_height
+
+
+class Leaf:
+    def __init__(self, image):
+        self.image = image
+
+    def positions(self, x, y, width, height):
+        yield (x, y, width, height, self.image)
+
+
+def render(tree, width, height, frame_width, color):
+    collg = Image.new('RGB', (width, height), color)
+    for (x, y, w, h, img) in tree.positions(0, 0, width, height):
+        resized_img = img.resize((int(w), int(h)), Image.LANCZOS)
+        collg.paste(resized_img, (int(x), int(y)))
+    return collg
+
+
 def collage(images, width, height, frame_width, color):
     '''Create a collage from multiple images.'''
-    raise NotImplementedError()
+    row = Row((1.0, Leaf(img)) for img in images[1:])
+    col = Column([(1.0, Leaf(images[0])), (1.0, row)])
+    return render(col, width, height, frame_width, color)
 
 
 @click.command(name='collage')
@@ -29,8 +76,10 @@ def cli_collage(width, height, frame_width, color):
     '''Create a collage from multiple images.'''
     click.echo('Initializing collage with parameters {}'.format(locals()))
 
-    def _collage(images):
-        images = list(images)
-        yield images[0][0], collage(images, width, height, frame_width, color)
+    def _collage(image_infos):
+        image_infos = list(image_infos)
+        images = [img for _, img in image_infos]
+        yield image_infos[0][0], \
+            collage(images, width, height, frame_width, color)
 
     return _collage
