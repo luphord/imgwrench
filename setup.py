d4882fbdb20aaa4cb42f62932259d7b9a24679ee
luphord <luphord@protonmail.com>
2020-07-24 10:01:20 +0200

    restrict Pillow version to fix test

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'Pillow>=5.0', 'numpy>=1.10']
+requirements = ['Click>=6.0', 'Pillow>=5.0, <7.2.0', 'numpy>=1.10']
 
 setup_requirements = []
 
