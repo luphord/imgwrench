b3795eef34c56eea9cd3448d7b7da16e6219166e
luphord <luphord@protonmail.com>
2020-08-08 15:53:04 +0200

    test landscape quad output size

--- tests/test_quad.py
+++ tests/test_quad.py
@@ -4,11 +4,20 @@ import unittest
 
 from click.testing import CliRunner
 
+from imgwrench.commands.quad import quad
+
+from .images import pixel1x1_img
 from .utils import execute_and_test_output_images
 
 
 class TestQuad(unittest.TestCase):
     '''Test for `quad` command.'''
+
+    def test_quad_size(self):
+        '''Test output sizes of quad.'''
+        _, out100x50 = quad([(None, pixel1x1_img)], 100, 50)
+        self.assertEqual(100, out100x50.size[0])
+        self.assertEqual(50, out100x50.size[1])
 
     def test_quad_output(self):
         '''Test output of quad command.'''
