86a691132198181b00739affb7b333542886ffb6
luphord <luphord@protonmail.com>
2022-11-12 20:51:06 +0100

    drop support for Python 3.6 and 3.7, but add support for Python 3.9 and 3.10

--- setup.py
+++ setup.py
@@ -26,9 +26,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description="A highly opinionated image processor for the commandline.",
     entry_points={
